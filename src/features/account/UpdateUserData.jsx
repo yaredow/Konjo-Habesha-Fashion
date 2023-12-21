70b635c14e5abbff9226db9947300a8545f18675
yaredow <yaredyilma11@gmail.com>
2023-12-21 17:00:58 +0200

    fixed the inset-0 problem

--- src/features/account/UpdateUserData.jsx
+++ src/features/account/UpdateUserData.jsx
@@ -3,7 +3,7 @@ import UpdatePassword from '../../ui/updatePassword';
 
 const UpdateUserData = () => {
   return (
-    <div className="flex mt-12 fixed inset-0 top-0 items-center justify-center h-screen bg-white dark:bg-gray-800">
+    <div className="flex top-0 items-center justify-center h-screen bg-white dark:bg-gray-800">
       <div className="bg-white dark:bg-gray-800 p-8 rounded-lg w-2/3">
         <UpdateData />
         <UpdatePassword />
